74aebfe87e8777d3beadaece248104c0509c1313
Keerthana Srinivasan <140654812+Keerthana07S@users.noreply.github.com>
2025-05-01 08:23:23 -0400

    Update current_modulation.cc

--- C++/current_modulation.cc
+++ C++/current_modulation.cc
@@ -1,34 +1,31 @@
-int mosfetPin = //mosfet pin #;  
-int voltageSensorPin = A1; //virtual pin for now. ideally, an actual voltage sensor would be connected to this. 
+int mosfetPin = 9; 
+int voltageSensorPin = A0; 
 
 float ISC = 0.683;  
-float V_oc = 25.0;  
+float V_oc = 8.0;  
+float beta = 8.0;
 
 void setup() {
   pinMode(mosfetPin, OUTPUT); 
-
   Serial.begin(9600);
 }
 
 void loop() {
   float voltage = (analogRead(voltageSensorPin) / 1023.0) * V_oc; 
 
-  float beta = 8.0;  
   float I_output = ISC * (1.0 - pow((voltage / V_oc), beta));
-  float I_LOSS = (ISC - I_output);
-  int pwmValue = int((I_LOSS / ISC) * 255.0); 
+  I_output = constrain(I_output, 0.0, ISC); 
 
+  int pwmValue = int((I_output / ISC) * 255.0);
   pwmValue = constrain(pwmValue, 0, 255);
 
   analogWrite(mosfetPin, pwmValue);
 
-  Serial.print("");
   Serial.print(voltage, 2); 
   Serial.print(",");
   Serial.print(I_output, 4); 
   Serial.print(",");
   Serial.println(pwmValue);
 
-  delay(100); 
-  
+  delay(100);
 }
