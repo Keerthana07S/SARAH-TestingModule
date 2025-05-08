62ceeb5a498fee14f15398d3f051d5abe08318e2
Keerthana Srinivasan <140654812+Keerthana07S@users.noreply.github.com>
2025-05-08 10:46:20 -0400

    Update photovoltaic_emulator.cc

--- C++/photovoltaic_emulator.cc
+++ C++/photovoltaic_emulator.cc
@@ -1,4 +1,3 @@
-#define tempPin A1
 #define voltageSensor A0
 #define voltageModify 9
 const float R1 = 10000.0;
@@ -6,29 +5,12 @@ const float R2 = 6800.0;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(tempPin, INPUT);
   pinMode(voltageSensor, INPUT);
   pinMode(voltageModify, OUTPUT);
-  String input = "Hello";
-}
-
-
-void setup() {
-  Serial.begin(9600);
-  pinMode(tempPin, INPUT);
-  pinMode(voltageSensor, INPUT);
-  pinMode(voltageModify, OUTPUT);
+  Serial.println("Voltage (V), Current (A), Battery (V)");
 }
 
 float G = 1000.0; // will be adding a function soon to obtain this from a radfet
-
-float obtain_temp() {
-  int reading = analogRead(tempPin);
-  float voltage = reading * (5000/1024.0);
-  float T_celsius = (voltage-500)/10;
-  float T_kelvin = T_celsius + 273.15;
-  return T_kelvin;
-}
 
 float I_sc = 0.683;
 int P_mpp = 12;
@@ -45,7 +27,6 @@ float n = 1.3;
 float q = 1.602e-19;
 float T_ref_kelvin = 298.15;
 float E_g = 1.1;
-float N_p = 1.0;
 
 float V = 0.0;
 float I = 0.0;
@@ -60,24 +41,25 @@ float calculate_voc(float I_ph, float I_s, float V_t){
   return V_oc;
 }
 
-float calculate_irs(float T_kelvin, float I_sc, float q, float n, float K) {
+float calculate_irs(float I_sc, float V_oc, float q, float n, float K, float T_kelvin) {
   float denom = exp((q * V_oc) / (n * K * T_kelvin)) - 1.0;
   float I_rs = I_sc / denom;
   return I_rs;
 }
 
 float calculate_is(float I_rs, float T_kelvin) {
-  float exponent = (-q * E_g / (n * K)) * ((1.0 / T_kelvin) - (1.0 / T_ref));
-  return I_rs * pow(T_kelvin / T_ref, 3) * exp(exponent);
+  float exponent = (-q * E_g / (n * K)) * ((1.0 / T_kelvin) - (1.0 / T_ref_kelvin));
+  return I_rs * pow(T_kelvin / T_ref_kelvin, 3) * exp(exponent);
 }
 
 float calculate_iph(float G, float T_kelvin, float T_ref_kelvin, float I_sc, float K_i) {
   float I_ph = G * (I_sc + K_i*(T_kelvin - T_ref_kelvin));
-  return I_ph
+  return I_ph;
 }
 
 float calculate_id(float I_s, float V_t, float V, float I) {
   float exp_arg = (V + I * R_s) / (n * V_t * N_s);
+  if (exp_arg > 80.0) exp_arg = 80.0;
   float I_d = I_s * (exp(exp_arg) - 1.0) * N_p;
   return I_d;
 }
@@ -87,56 +69,57 @@ float calculate_ish(float V, float I) {
   return I_sh;
 }
 
-float T_op = obtain_temp();
+float T_op = 298.15;
 float V_t = calculate_vt(T_op);
 float I_ph = calculate_iph(G, T_op, T_ref_kelvin, I_sc,  K_i);
-float V_oc = 25.0;
-float I_rs = calculate_irs(I_sc, V_oc, q, n, k, T_op);
+float I_rs = calculate_irs(I_sc, 25.0, q, n, K, T_op);
 float I_s = calculate_is(I_rs, T_op);
 float V_oc = calculate_voc(I_ph, I_s, V_t);
 
-float calculate_current(float I_ph, float N_p, float I_d, float I_sh) {
+float calculate_current(float I_ph, int N_p, float I_d, float I_sh) {
   float I = I_ph * N_p - I_d - I_sh;
   return I;
 }
 
-float I = 0.0;
-
 void loop() {
-  if (Serial.available()) {
-    String input = Serial.readStringUntil('\n');
+  float T_op = 1000.15;  
+  float Vt = calculate_vt(T_op);
+  Serial.println(Vt);
+  Serial.print("V_t");
+  float I_ph = calculate_iph(G, T_op, T_ref_kelvin, I_sc, K_i);
+  Serial.println(I_ph);
+  Serial.print("I_ph");
 
-    if (input == "Hello") {
-      float T_op = obtain_temp();
-      float V_t = calculate_vt(T_op);
-      float I_ph = calculate_iph(G, T_op, I_sc, K_i);
+  float V_oc_guess = 25.0;  // safe estimate
+  float I_rs = calculate_irs(I_sc, V_oc_guess, q, n, k, T_op);
+  Serial.println(I_rs);
+  Serial.print("I_rs");
+  float I_s = calculate_is(I_rs, T_op);
+  Serial.println(I_s);
+  Serial.print("I_s");
+  float V_oc = 25.0;
 
-      float V_oc_guess = 25.0; // we set an initial value for V_oc until we can calculate the actual value
-      float I_rs = calculate_irs(I_sc, V_oc_guess, q, n, k, T_op);
-      float I_s = calculate_is(I_rs, T_op);
-      float V_oc = calculate_voc(I_ph, I_s, V_t); // we calculate the actual value for V_oc
+  float I = I_sc;
 
-      Serial.println("Voltage (V), Current (A), Battery (V)");
-      float I = I_sc; // the current always initially starts on I_sc
-      
-      for (int v = 0; v <= V_oc; v += 0.5) {
-        float I_d = calculate_id(I_s, V_t, v, I);
-        float I_sh = calculate_ish(v, I);
-        I = calculate_current(I_ph, I_d, I_sh); // we calculate the new current
+  for (float v = 0; v <= V_oc; v += 0.5) {
+    float I_d = calculate_id(I_s, Vt, v, I);
+    Serial.println(I_d);
+    float I_sh = calculate_ish(v, I);
+    Serial.println(I_sh);
+    I = calculate_current(I_ph, N_p, I_d, I_sh);
+    Serial.println(I);
 
-        Serial.print(v);
-        Serial.print(", ");
-        Serial.print(I);
-        Serial.print(", ");
+    Serial.print(v);
+    Serial.print(", ");
+    Serial.print(I);
+    Serial.print(", ");
 
-        analogWrite(voltageModify, 255);
-        int sensorValue = analogRead(voltageSensor);
-        float voltageAtPin = sensorValue * (5.0 / 1023.0); // voltage at A0
-        float batteryVoltage = voltageAtPin * ((R1 + R2) / R2); // undo the voltage divider
-        Serial.print(batteryVoltage);
-        Serial.println();
-        delay(1000);
-      }
-    }
+    analogWrite(voltageModify, 255);
+    int sensorValue = analogRead(voltageSensor);
+    float voltageAtPin = sensorValue * (5.0 / 1023.0);
+    float batteryVoltage = voltageAtPin * ((R1 + R2) / R2);
+    Serial.print(batteryVoltage);
+    Serial.println();
+    delay(1000);
   }
 }
