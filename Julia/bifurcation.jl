2dc65ddcbf1c971aaa71a29c4ba22f5daecd517f
Keerthana Srinivasan <140654812+Keerthana07S@users.noreply.github.com>
2025-06-03 10:33:38 -0400

    Update bifurcation.jl

--- Julia/bifurcation.jl
+++ Julia/bifurcation.jl
@@ -1,23 +1,31 @@
-using LinearAlgebra
+#author: keerthana srinivasan 
+#date of completion: 04/18/2025
+#description: using the bifurcation method, we can detect system instability on our electrical power subsystems through leveraging the determinant and trace of a certain jacobian
 
-L_eq = 5
-R_eq = 5
-V_ref = 12
-v_bus = 12
-i_s = 2
-C = 238
-P = 5.8
-R = 106
-delta_t = 1.0
+using LinearAlgebra #for matrix and numerical operations 
 
+#givens 
+L_eq = 5 #quivalent inductance 
+R_eq = 5 #equivalent resistance 
+V_ref = 12 #reference voltage 
+v_bus = 12 #bus voltage 
+i_s = 2 #bus current 
+C = 238 #capacitance 
+P = 5.8 #power 
+R = 106 #resistance 
+delta_t = 1.0 #change in time 
+
+#function f() finds the change in bus current over time.
 function f(i_s, v_bus, L_eq, V_ref, R_eq)
     return di_ds = (1/L_eq)*(V_ref - v_bus - (R_eq*i_s))
 end
 
+#function g() finds the change in bus voltage over time 
 function g(i_s, v_bus, C, R, P)
     return dvbus_ds = (1/C)*(i_s-(v_bus/R)-(P/v_bus))
 end
 
+#the runge kutta function finds the bus current and voltage over various time steps through integration over time steps. 
 function runge_kutta(i_s, v_bus, L_eq, V_ref, R_eq, C, R, P)
     ki_is = f(i_s, v_bus,L_eq, V_ref, R_eq)
     ki_vs = g(i_s, v_bus, C, R, P)
@@ -34,6 +42,7 @@ function runge_kutta(i_s, v_bus, L_eq, V_ref, R_eq, C, R, P)
     return i_s, v_bus
 end
 
+#you can use a jacobian to find system instability. you can do this through finding the determinant and trace of the jacobian.
 function fault_jacobian(v_bus, L_eq, R_eq, C, R, P)
     J = [(-R_eq/L_eq)  (-1/L_eq); (1/C)  (1/C) * ((P/(v_bus)^2) - (1/R))]
     
@@ -45,6 +54,7 @@ function fault_jacobian(v_bus, L_eq, R_eq, C, R, P)
     println("Trace: ", trace_J)
     println("Determinant: ", det_J)
 
+    #conditions for determinant and trace of the jacobian
     if det_J > 0 && trace_J < 0
         stability = true
     end
@@ -56,5 +66,6 @@ function fault_jacobian(v_bus, L_eq, R_eq, C, R, P)
     println("System Stability: ", stability)
 end
 
+#get your results!
 runge_kutta(i_s, v_bus, L_eq, V_ref, R_eq, C, R, P)
 fault_jacobian(v_bus, L_eq, R_eq, C, R, P)
