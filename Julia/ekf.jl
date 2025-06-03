acedda1c9c61a553590a2d8756b452d5a0f2dd08
Keerthana Srinivasan <140654812+Keerthana07S@users.noreply.github.com>
2025-06-03 10:36:30 -0400

    Update ekf.jl

--- Julia/ekf.jl
+++ Julia/ekf.jl
@@ -1,22 +1,26 @@
-include("bifurcation.jl")
-using HypothesisTests, Plots, StatsBase
-using LinearAlgebra
+#author: keerthana srinivasan
+#date of completion: 04/18/25
+#description: extended kalman filter for fault classification through the variance of the residual
 
-# Our givens
+include("bifurcation.jl") #have old bifurcation file for certain calculations
+using HypothesisTests, Plots, StatsBase #run your variance tests
+using LinearAlgebra #for matrix and other numerical operations
+
+#our givens
 i_s = 2.0
 v_bus = 12.0
 
-# Initial state matrix
+#initial state matrix
 global A = [(1)  (0) ;  
             (0)  (1)] 
 global x_i = [i_s ; v_bus]
 
-# Control input covariance matrix (normalized)
+#control input covariance matrix (normalized)
 global B = [(0.07790571296205358) (-476.73291832983836) (0.06451957436481184) (-2.1030957120413913) (-2.203787730050049) (-0.023300191156435873) (0.0007169397964113035) (-0.004180565548674369) ; 
             (-0.0028702910932419654) (0.6927547150205023) (0.008879631830097259) (0.011704044977636538) (0.009279933443372732) (0.1123438976754724) (-0.003659937896493428) (0.020843190291931803)]
-global B = B ./ maximum(abs.(B)) # Normalize B
+global B = B ./ maximum(abs.(B)) #normalize B
 
-# Control inputs
+#control inputs
 global u_i =[R_eq;
             L_eq;
             V_ref;
@@ -26,15 +30,15 @@ global u_i =[R_eq;
             P;
             R]
 
-# Measurement matrix
+#measurement matrix
 global H = [(1)  (0) ;  
             (0)  (1)]
 
-# Number of iterations
+#number of iterations
 global n = 100
 residuals = Float64[]
 
-# Regressors for White Test
+#regressors for White Test
 X = []
 Y = ones(100, 1)
 
@@ -49,7 +53,7 @@ function predicted_state(x_i::Vector{Float64}, u::Vector{Float64}, R_eq)
     return x_f
 end
 
-# Residual calculation
+#residual calculation
 function residual_calculation(x_i, x_ii)
     z_f = H*x_ii
     z_i = H*x_i
@@ -57,7 +61,7 @@ function residual_calculation(x_i, x_ii)
     return V_i 
 end
 
-# Run the simulation
+#run the simulation
 for t in 1:n
     global i_s, v_bus = runge_kutta(i_s, v_bus, L_eq, V_ref, R_eq, C, R, P)
     x_ii = predicted_state(x_i, u_i, R_eq)
