d8d526a11cddf596b2c263d37eb3c044174f1ff9
Keerthana Srinivasan <140654812+Keerthana07S@users.noreply.github.com>
2025-06-05 08:31:47 -0400

    Update qmcmc.jl

--- Julia/qmcmc.jl
+++ Julia/qmcmc.jl
@@ -1,10 +1,14 @@
-using Pkg
-using Statistics
-using QSWalk
-using LightGraphs
-using LinearAlgebra
-using Random
-using Plots  
+#author: keerthana srinivasan
+#date of completion: 4/18/2025
+#description: QMCMC for fault localization in spacecraft electrical power subsystems. 
+
+using Pkg #install libraries
+using Statistics #make statistical calculations
+using QSWalk #quantum walks
+using LightGraphs #graphing!
+using LinearAlgebra #numerical and matrix calculations
+using Random #random number generation
+using Plots #plotting graphs
 
 dim = 151 
 midpoint = ceil(Int, dim / 2) 
@@ -13,15 +17,19 @@ timepoint = 100.0
 
 adjacency = adjacency_matrix(PathGraph(dim))
 
+#energy function for markov chain propagation
 function energy(s::Vector{Int}, J::Matrix{Float64}, h::Vector{Float64})
     return -dot(s, J * s) - dot(h, s)  
 end
 
+#spin states
 s = rand([-1, 1], dim) 
 
+#J and h matrices to define problem specification in energy function
 J = Matrix(adjacency) .* (rand(dim, dim) .- 0.5)  
 h = zeros(dim)  
 
+#define J and h matrix values for nodes j and k based on voltage and current thresholds
 function update_fault_params!(J::Matrix{Float64}, h::Vector{Float64}, V::Vector{Float64}, I::Vector{Float64}, V_threshold::Float64, I_threshold::Float64)
     mean_I = mean(I)
     for j in 1:dim
@@ -39,6 +47,7 @@ function update_fault_params!(J::Matrix{Float64}, h::Vector{Float64}, V::Vector{
     end
 end
 
+#choose between spin states during monte carlo process
 function boltzmann_sample(J::Matrix{Float64}, h::Vector{Float64}, T::Float64, samples::Int=100)
     sampled_states = [rand([-1, 1], dim) for _ in 1:samples] 
     energies = [energy(s, J, h) for s in sampled_states]
@@ -49,10 +58,12 @@ function boltzmann_sample(J::Matrix{Float64}, h::Vector{Float64}, T::Float64, sa
     return best_state
 end
 
+#one step in the monte carlo process
 function qmcmc_step(s::Vector{Int}, J::Matrix{Float64}, h::Vector{Float64}, T::Float64)
     return boltzmann_sample(J, h, T, 100) 
 end
 
+#count the number of times we visit each area to build the boltzmann distribution (approximation)
 function evolve_qmcmc(s::Vector{Int}, J::Matrix{Float64}, h::Vector{Float64}, V::Vector{Float64}, I::Vector{Float64}, V_threshold::Float64, I_threshold::Float64, T::Float64, steps::Int)
     fault_counter = zeros(dim)  
 
@@ -67,17 +78,21 @@ function evolve_qmcmc(s::Vector{Int}, J::Matrix{Float64}, h::Vector{Float64}, V:
     return s, fault_prob
 end
 
+#set voltage and current parameters
 V = rand(dim) * 50 
 I = rand(dim) * 5   
 
+#set voltage and current threshold
 V_threshold = 20.0  
 I_threshold = 1.0  
 
+#find fault probability distribution
 final_state, fault_prob = evolve_qmcmc(s, J, h, V, I, V_threshold, I_threshold, 0.1, 1000)
-
+#find faulty nodes
 faulty_nodes = findall(x -> x == -1, final_state)
 println("Faulty nodes identified: ", faulty_nodes)
 
+#graph probability distribution
 plot(1:dim, fault_prob, lw=2, label="Fault Probability", color=:blue)
 scatter!(1:dim, fault_prob, marker=:circle, color=:red, label="Nodes")
 xlabel!("Node Index")
